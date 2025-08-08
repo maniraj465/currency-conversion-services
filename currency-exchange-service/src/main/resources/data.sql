f6e8804da3264bacc6218f56f3ff2d5cef84b1d5
maniraj465 <38633966+maniraj465@users.noreply.github.com>
2025-08-08 14:05:26 +0530

    latest

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,7 +1,10 @@
+-- noinspection SqlDialectInspectionForFile
+
+-- noinspection SqlNoDataSourceInspectionForFile
 
 
 -- Insert data in currency_exchange table
-insert into currency_exchange(id, from_currency, to_currency, conversion_multiple, environment) values(10001, 'USD', 'INR', 82, '8000');
+insert into currency_exchange(id, from_currency, to_currency, conversion_multiple, environment) values(10001, 'USD', 'INR', 86, '8000');
 insert into currency_exchange(id, from_currency, to_currency, conversion_multiple, environment) values(10002, 'EUR', 'INR', 90, '8001');
 insert into currency_exchange(id, from_currency, to_currency, conversion_multiple, environment) values(10003, 'AUD', 'INR', 81, '8000');
 insert into currency_exchange(id, from_currency, to_currency, conversion_multiple, environment) values(10004, 'CAD', 'INR', 55, '8000');
